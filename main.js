9e1ddc572e06d68fd7820d229f16ba748a3e93b5
Davis Uhlig <duhlig@Daviss-MacBook-Pro.local>
2017-06-12 23:17:37 -0400

    added eval function

--- main.js
+++ main.js
@@ -2,32 +2,39 @@ const sevenBtn = document.querySelector("#seven");
 const eightBtn = document.querySelector("#eight");
 let btnList = document.querySelectorAll(".squares");
 let display = document.getElementById("text-content");
-var evalArray = [];
-var operatorArray = [];
+var solution;
+
 for (let i = 0; i < btnList.length; i++) {
   let btn = btnList[i];
 
-  btn.addEventListener("click", function addDisplay(event) {
-  console.log(btn);
-  
-  display.innerHTML += btn.innerHTML;
-  if (btn.classList.contains("clear")){
+  btn.addEventListener("click", addDisplay)
+
+function addDisplay(event){
+  if (btn.classList.contains("numbers")){
+    display.innerHTML += btn.innerHTML;
+
+  }
+  else if (btn.classList.contains("clear")){
     var clearInput = "";
     display.innerHTML = clearInput;
-    evalArray=[];
   }
 
-  else if (btn.classList.contains("operator")) {
-    evalArray.push(display.innerHTML);
-    operatorArray.push(btn.innerHTML);
-    console.log(evalArray);
+  // else if (btn.classList.contains("operator")) {
+  //   evalArray.push(display.innerHTML);
+  //   operatorArray.push(btn.innerHTML);
+  //   console.log(evalArray);
+  // }
+
+  else if (btn.classList.contains("equals")) {
+    solution = eval(display.innerHTML);
+    console.log(solution);
+    display.innerHTML = solution;
   }
 
 
-})
-// function equal(string) {
-//   newDisplay = display.innerHTML;
-//   newDisplay = eval(newDisplay);
-//   display.innerHTML = newDisplay;
+
+
+}
+
 
 }
